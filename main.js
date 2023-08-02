9dd4dfb27623ba9f507fe6151caefc720750fed0
Verna0214 <yuhsichen0214@gmail.com>
2023-08-02 20:55:07 +0800

    Fix: cards footer

--- main.js
+++ main.js
@@ -16,11 +16,11 @@ const view = {
         <div class="card border-warning" style="min-height: 150px;">
           <div class="card-body">
             <p class="card-text">${data}</p>
-            <div>
-              <i class="fa-solid fa-pen me-2"></i>
-              <i class="fa-solid fa-bolt fa-lg me-2" style="color: #510ecd;"></i>
-              <i class="fa-solid fa-xmark fa-xl" style="color: #ff0000;"></i>
-            </div>
+          </div>
+          <div class="card-footer d-flex justify-content-end align-items-center">
+            <i class="fa-solid fa-pen me-2"></i>
+            <i class="fa-solid fa-bolt fa-lg me-2" style="color: #510ecd;"></i>
+            <i class="fa-solid fa-xmark fa-xl" style="color: #ff0000;"></i>
           </div>
         </div>
       </div>
